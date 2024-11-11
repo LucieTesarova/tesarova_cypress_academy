9072c25f2c88a249785010c1e8cc2ed0db06a625
tesaroval <lucie.tesarova@peopleinneed.net>
2024-11-11 20:28:33 +0100

    create project and task exercise

--- cypress/page-objects/pmtool/create_project_modal.js
+++ cypress/page-objects/pmtool/create_project_modal.js
@@ -1,7 +1,22 @@
+import { TasksPage } from "./projects/tasks_page";
+
 export class CreateProjectModal {
   constructor() {
+    this.prioritySelect = 'div[data-testid="Priority"] select';
+    this.statusSelect = 'div[data-testid="Status"] select';
     this.nameInput = 'div[data-testid="Name"] input';
+    this.startDateInput = 'div[data-testid="Start Date"] input';
     this.saveButton = 'button[type="submit"]';
+  }
+
+  selectPriority(priority) {
+    cy.get(this.prioritySelect).select(priority);
+    return this;
+  }
+
+  selectStatus(status) {
+    cy.get(this.statusSelect).select(status);
+    return this;
   }
 
   typeName(projectName) {
@@ -9,8 +24,13 @@ export class CreateProjectModal {
     return this;
   }
 
+  typeStartDate(startDate) {
+    cy.get(this.startDateInput).type(startDate);
+    return this;
+  }
+
   clickSave() {
     cy.get(this.saveButton).click();
-    //TODO add returned PageObject after implementation
+    return new TasksPage();
   }
 }
