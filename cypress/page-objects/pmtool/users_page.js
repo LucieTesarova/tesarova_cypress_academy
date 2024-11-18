1b9653a65fcfbba25a303b0f3636e7e83c526ee1
tesaroval <lucie.tesarova@peopleinneed.net>
2024-11-18 20:54:24 +0100

    Atomic tests II - custom element, turn off test isolation

--- cypress/page-objects/pmtool/users_page.js
+++ cypress/page-objects/pmtool/users_page.js
@@ -1,9 +1,11 @@
+import { customElement } from "../../helpers/custom_element";
 import { HeaderSection } from "./common/header_section";
 
 export class UsersPage extends HeaderSection {
   constructor() {
     super("module=items/items&path=1");
-    this.projectsHeader = ".page-title";
+    this.usersHeader = customElement(".page-title");
+    this.addUserButton = customElement('[test_id="Add User"]');
     //cy.get(this.projectsHeader).should("contain.text", "Users");
   }
 }
