aca7531479023f4d80b1baf0bd6ce1600c8f2531
tesaroval <lucie.tesarova@peopleinneed.net>
2024-11-30 12:14:39 +0100

    Bonus HW - API test - create bank account

--- cypress/page-objects/tegb/accounts_page.js
+++ cypress/page-objects/tegb/accounts_page.js
@@ -3,10 +3,16 @@ import { customElement } from "../../helpers/custom_element.js";
 export class AccountsPage {
   constructor() {
     this.titleHeader = customElement('[data-testid="title"]');
+    this.balance = customElement('[data-testid="account_balance"]');
   }
 
   headerHaveText(headerText) {
     this.titleHeader.haveText(headerText);
+    return this;
+  }
+
+  balanceContainText(balance) {
+    this.balance.containText(balance);
     return this;
   }
 }
